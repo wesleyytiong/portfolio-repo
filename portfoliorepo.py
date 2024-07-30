a1d5ccb9177177d9c9391380fc982a65d1f26ddb
wesleyytiong <wesleyytiong@gmail.com>
2024-07-29 19:42:49 -0500

    Update portfoliorepo.py
    Importing data, based on 3 stocks within a 1 year period and calculating returns/mean returns/ and covariance matrix

--- portfoliorepo.py
+++ portfoliorepo.py
@@ -1,3 +1,30 @@
-#Portfolio
-r = print('This is a Portolio from website')
+"""
+Portfolio Optimization
+"""
 
+import numpy as np
+import datetime as dt
+import yfinance as yf
+from pandas_datareader import data as pdr
+
+#Import data
+def getData(stocks, start, end):
+    stockData = yf.download(stocks, start = start, end = end)
+    stockData = stockData['Close']
+    
+#From data, output returns as percent change, mean returns, and covariance matrix
+    returns = stockData.pct_change()
+    meanReturns = returns.mean()
+    covMatrix = returns.cov()
+    return meanReturns, covMatrix
+
+
+#Define parameters
+stockList = ['CBA', 'BHP', 'TLS']
+stocks = [stock+'.AX' for stock in stockList]
+
+endDate = dt.datetime.now()
+startDate = endDate - dt.timedelta(days=365)
+
+data = getData(stocks, start=startDate, end=endDate)
+print(data)
